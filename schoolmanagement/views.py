1ea693e9984d6a11805130da67e1742ff19f2a6e
BalayetHosain <balayet.kafo@gmail.com>
2021-06-10 15:09:15 +0600

    initial recaptcha

--- schoolmanagement/views.py
+++ schoolmanagement/views.py
@@ -1,6 +1,7 @@
   
 import datetime
-
+import requests
+import json
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
@@ -20,14 +21,25 @@ def showLoginPage(request):
 
 def doLogin(request):
     if request.method!="POST":
-        return HttpResponse("<h2>Method Not Allow</h2>")
+        return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        user = EmailBackEnd.authenticate(request, username=request.POST.get("email"),password=request.POST.get("password"))
+        captcha_token=request.POST.get("g-recaptcha-response")
+        cap_url="https://www.google.com/recaptcha/api/siteverify"
+        cap_secret="6LegIiQbAAAAAKmegL1VfrD5n4kltezfC1oiWakm"
+        cap_data={"secret":cap_secret,"response":captcha_token}
+        cap_server_response=requests.post(url=cap_url,data=cap_data)
+        cap_json=json.loads(cap_server_response.text)
+
+        if cap_json['success']==False:
+            messages.error(request,"Invalid Captcha Try Again")
+            return HttpResponseRedirect("/")
+
+        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
         if user!=None:
             login(request,user)
-            if user.user_type == "1":
+            if user.user_type=="1":
                 return HttpResponseRedirect('/admin_home')
-            elif user.user_type == "2":
+            elif user.user_type=="2":
                 return HttpResponseRedirect(reverse("staff_home"))
             else:
                 return HttpResponseRedirect(reverse("student_home"))
